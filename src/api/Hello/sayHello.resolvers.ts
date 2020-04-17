ea04b23576f65ad32337ac3600d046ab50b09ecb
sanek306 <sanek306@gmail.com>
2020-04-17 22:00:51 +0300

    add types for arguments of query

--- src/api/Hello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello.resolvers.ts
@@ -1,11 +1,11 @@
-import {Greeting} from "../../types/graph";
+import {SayHelloQueryArgs, SayHelloResponse} from "../../types/graph";
 
 const resolvers = {
     Query: {
-        sayHello:  (): Greeting => {
+        sayHello:  (_, args: SayHelloQueryArgs, context): SayHelloResponse => {
             return {
                 error: false,
-                text: "Hello"
+                text: `Hello ${args.name}`
             }
         }
     }
